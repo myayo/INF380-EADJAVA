bb1c323e9d222245c6c1fa26bdf2019231a26362
HanzhiWang1105 <wanghanzhiemilie@gmail.com>
2014-06-13 12:57:51 +0000

    modify FileService

--- src/main/java/com/inf380/ead/service/FileService.java
+++ src/main/java/com/inf380/ead/service/FileService.java
@@ -1,33 +1,52 @@
 package com.inf380.ead.service;
 import java.io.File;
-public class FileService {
-	public String filename;
-	public String javaname1;
-	public FileService(String fn,String jn){
-		filename=fn;
-		javaname1=jn;
-	}
-	public void showname(){
-		System.out.println("filename= "+filename+" javaname= "+javaname1);
-	}
-	public void createfile(){
-		File file_name=new File(filename);
-		
-		file_name.mkdirs();
-	}
-public void createjava(){
-	String javafile1=filename+"/"+javaname1+".java";
-	File java_name=new File(javafile1);
-	try
-	{
-		java_name.createNewFile();
-	}
-	catch(java.io.IOException ex)
-	{
-		ex.printStackTrace();
-	}
-}
-}
+import java.io.FileWriter;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+public class FileService {
+	
+	/**
+	 * 
+	 * @param name
+	 * @param pathname
+	 * @param type
+	 * @param content
+	 * @throws IOException 
+	 */
+	public void createOrUpdateFile(String pathname, String type, String content) throws IOException{
+		File file= new File(pathname);
+		//create or save directory
+		if(type.equals("directory")){
+			if(!file.exists()){
+				file.mkdir();
+			}
+			}else if(type.equals("file")){
+				//create File
+				if(!file.exists()){
+					file.createNewFile();
+				}
+				//add content
+				FileWriter writer = new FileWriter(file);
+				writer.write(content);
+				writer.flush();
+				writer.close();
+			}
+	}
+	
+	/**
+	 * 
+	 * @param pathname
+	 * @throws IOException
+	 */
+	public void deleteFile(String pathname) throws IOException{
+		File file= new File(pathname);
+		if(file.isDirectory()){
+			FileUtils.deleteDirectory(file);
+		}else{
+			file.delete();
+		}
+	}
+}
 
 
